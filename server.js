c268028bb07d261b226366a464ff784c87409ba2
Alpay Celik <alpaycelik91@googlemail.com>
2023-10-14 19:54:34 +0200

    added routers, controller and models

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ import express from "express";
 import morgan from "morgan";
 import * as dotenv from "dotenv";
 dotenv.config();
+
+import { nanoid } from "nanoid";
+
+import jobRouter from "./routers/jobRouter.js";
+
+import mongoose from "mongoose";
+
+const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-import { nanoid } from "nanoid";
+app.use("/api/v1/jobs", jobRouter);
 
-const app = express();
 let jobs = [
   { id: nanoid(), company: "apple", position: "front-end" },
   { id: nanoid(), company: "google", position: "back-end" },
@@ -37,14 +44,13 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-// CREATE JOB
-
 // GET ALL JOBS
 app.get("/api/v1/jobs", (req, res) => {
   // console.log(jobss);
   res.status(200).json({ jobs });
 });
 
+// CREATE JOB
 app.post("/api/v1/jobs", (req, res) => {
   const { company, position } = req.body;
   if (!company || !position) {
@@ -102,6 +108,12 @@ app.delete("/api/v1/jobs/:id", (req, res) => {
 });
 
 const port = process.env.PORT || 5100;
-app.listen(port, () => {
-  console.log(`server running on PORT ${port}....`);
-});
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}....`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
