1ba81b6f1e1b86eddd672d6136f46e944d0ccf5a
AlpayC <alpaycelik91@googlemail.com>
2023-12-31 11:03:47 +0100

    added client and all components, frontend done

--- client/src/pages/DeleteJob.jsx
+++ client/src/pages/DeleteJob.jsx
@@ -1,5 +1,17 @@
-const DeleteJob = () => {
-  return <h1>Delete Job</h1>;
-};
+import { toast } from 'react-toastify';
+import customFetch from '../utils/customFetch';
+import { redirect } from 'react-router-dom';
 
-export default DeleteJob;
+export const action =
+  (queryClient) =>
+  async ({ params }) => {
+    try {
+      await customFetch.delete(`/jobs/${params.id}`);
+      queryClient.invalidateQueries(['jobs']);
+
+      toast.success('Job deleted successfully');
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+    }
+    return redirect('/dashboard/all-jobs');
+  };
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,15 +1,17 @@
-import main from "../assets/images/main.svg";
-import { Link } from "react-router-dom";
-import logo from "../assets/images/logo.svg";
-import Wrapper from "../assets/wrappers/LandingPage";
+import styled from 'styled-components';
+import Wrapper from '../assets/wrappers/LandingPage';
+import main from '../assets/images/main.svg';
+import { Link } from 'react-router-dom';
+import { Logo } from '../components';
+
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="jobify" className="logo" />
+        <Logo />
       </nav>
-      <div className="container page">
-        <div className="info">
+      <div className='container page'>
+        <div className='info'>
           <h1>
             job <span>tracking</span> app
           </h1>
@@ -19,14 +21,14 @@ const Landing = () => {
             quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
             narwhal.
           </p>
-          <Link to="/register" className="btn register-link">
+          <Link to='/register' className='btn register-link'>
             Register
           </Link>
-          <Link to="/login" className="btn">
+          <Link to='/login' className='btn '>
             Login / Demo User
           </Link>
         </div>
-        <img src={main} alt="job hunt" className="img main-img" />
+        <img src={main} alt='job hunt' className='img main-img' />
       </div>
     </Wrapper>
   );
